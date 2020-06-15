819c199b6ccd9435a431018b92ffbf5f5bf97432
Jordan Speicher <jordan@jspeicher.com>
2020-06-15 10:15:24 -0500

    Various updates

--- snapserver/rootfs/etc/cont-init.d/snapserver.sh
+++ snapserver/rootfs/etc/cont-init.d/snapserver.sh
@@ -8,6 +8,16 @@ mkdir -p /share/snapcast
 
 CONFIG_FILE=/etc/snapserver.conf
 
+config_or_default() {
+    local config="$1"
+    local default="$2"
+    if bashio::config.has_value "$config"; then
+        echo $(bashio::config "$config");
+    else
+        echo $default
+    fi
+}
+
 if ! bashio::fs.file_exists '/etc/snapserver.conf'; then
     touch /etc/snapserver.conf ||
         bashio::exit.nok "Could not create snapserver.conf file on filesystem"
@@ -17,26 +27,24 @@ bashio::log.info "Populating snapserver.conf..."
 # Start creation of configuration
 
 echo "[stream]" > "${CONFIG_FILE}"
-for stream in $(bashio::config 'stream.streams'); do
+for stream in $(config_or_default 'stream.streams' ''); do
     echo "stream = ${stream}" >> "${CONFIG_FILE}"
 done
-echo "buffer = $(bashio::config 'stream.buffer')" >> "${CONFIG_FILE}"
-echo "codec = $(bashio::config 'stream.codec')" >> "${CONFIG_FILE}"
-echo "send_to_muted = $(bashio::config 'stream.send_to_muted')" >> "${CONFIG_FILE}"
-echo "sampleformat = $(bashio::config 'stream.sampleformat')" >> "${CONFIG_FILE}"
+echo "buffer = $(config_or_default 'stream.buffer' 1000)" >> "${CONFIG_FILE}"
+echo "codec = $(config_or_default 'stream.codec' flac)" >> "${CONFIG_FILE}"
+echo "send_to_muted = $(config_or_default 'stream.send_to_muted' 'false')" >> "${CONFIG_FILE}"
+echo "sampleformat = $(config_or_default 'stream.sampleformat' '48000:16:2')" >> "${CONFIG_FILE}"
 
 echo "[http]" >> "${CONFIG_FILE}"
-echo "enabled = $(bashio::config 'http.enabled')" >> "${CONFIG_FILE}"
-echo "doc_root = $(bashio::config 'http.docroot')" >> "${CONFIG_FILE}"
+echo "enabled = $(config_or_default 'http.enabled' 'false')" >> "${CONFIG_FILE}"
+echo "doc_root = $(config_or_default 'http.docroot' '')" >> "${CONFIG_FILE}"
 
 echo "[tcp]" >> "${CONFIG_FILE}"
-echo "enabled = $(bashio::config 'tcp.enabled')" >> "${CONFIG_FILE}"
+echo "enabled = $(config_or_default 'tcp.enabled' 'true')" >> "${CONFIG_FILE}"
 
 echo "[logging]" >> "${CONFIG_FILE}"
-echo "debug = $(bashio::config 'logging.enabled')" >> "${CONFIG_FILE}"
+echo "debug = $(config_or_default 'logging.enabled' 'false')" >> "${CONFIG_FILE}"
 
 echo "[server]" >> "${CONFIG_FILE}"
-echo "threads = $(bashio::config 'server.threads')" >> "${CONFIG_FILE}"
-
-echo "[server]" >> "${CONFIG_FILE}"
-echo "datadir = $(bashio::config 'server.datadir')" >> "${CONFIG_FILE}"
+echo "threads = $(config_or_default 'server.threads' '-1')" >> "${CONFIG_FILE}"
+echo "datadir = /share/snapcast/" >> "${CONFIG_FILE}"
