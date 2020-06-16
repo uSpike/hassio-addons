a53872837cfb6cc01df30ad06fffe1f907de06dd
Jordan Speicher <jordan@jspeicher.com>
2020-06-16 09:16:33 -0500

    Various updates

--- snapserver/rootfs/etc/cont-init.d/snapserver.sh
+++ snapserver/rootfs/etc/cont-init.d/snapserver.sh
@@ -18,33 +18,36 @@ config_or_default() {
     fi
 }
 
+CONF_BUFFER=$(config_or_default 'stream.buffer' 1000)
+CONF_CODEC=$(config_or_default 'stream.codec' flac)
+CONF_SEND_TO_MUTED=$(config_or_default 'stream.send_to_muted' 'false')
+CONF_SAMPLEFORMAT=$(config_or_default 'stream.sampleformat' '48000:16:2')
+
 if ! bashio::fs.file_exists '/etc/snapserver.conf'; then
     touch /etc/snapserver.conf ||
         bashio::exit.nok "Could not create snapserver.conf file on filesystem"
 fi
-bashio::log.info "Populating snapserver.conf..."
 
-# Start creation of configuration
+bashio::log.info "Populating snapserver.conf..."
 
 echo "[stream]" > "${CONFIG_FILE}"
 for stream in $(config_or_default 'stream.streams' ''); do
     echo "stream = ${stream}" >> "${CONFIG_FILE}"
 done
-echo "buffer = $(config_or_default 'stream.buffer' 1000)" >> "${CONFIG_FILE}"
-echo "codec = $(config_or_default 'stream.codec' flac)" >> "${CONFIG_FILE}"
-echo "send_to_muted = $(config_or_default 'stream.send_to_muted' 'false')" >> "${CONFIG_FILE}"
-echo "sampleformat = $(config_or_default 'stream.sampleformat' '48000:16:2')" >> "${CONFIG_FILE}"
+echo "buffer = ${CONF_BUFFER}" >> "${CONFIG_FILE}"
+echo "codec = ${CONF_CODEC}" >> "${CONFIG_FILE}"
+echo "send_to_muted = ${CONF_SEND_TO_MUTED}" >> "${CONFIG_FILE}"
+echo "sampleformat = ${CONF_SAMPLEFORMAT}" >> "${CONFIG_FILE}"
 
 echo "[http]" >> "${CONFIG_FILE}"
-echo "enabled = $(config_or_default 'http.enabled' 'false')" >> "${CONFIG_FILE}"
-echo "doc_root = $(config_or_default 'http.docroot' '')" >> "${CONFIG_FILE}"
+echo "enabled = false" >> "${CONFIG_FILE}"
 
 echo "[tcp]" >> "${CONFIG_FILE}"
-echo "enabled = $(config_or_default 'tcp.enabled' 'true')" >> "${CONFIG_FILE}"
+echo "enabled = true" >> "${CONFIG_FILE}"
 
 echo "[logging]" >> "${CONFIG_FILE}"
-echo "debug = $(config_or_default 'logging.enabled' 'false')" >> "${CONFIG_FILE}"
+echo "debug = false" >> "${CONFIG_FILE}"
 
 echo "[server]" >> "${CONFIG_FILE}"
-echo "threads = $(config_or_default 'server.threads' '-1')" >> "${CONFIG_FILE}"
+echo "threads = -1" >> "${CONFIG_FILE}"
 echo "datadir = /share/snapcast/" >> "${CONFIG_FILE}"
